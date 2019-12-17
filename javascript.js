30c5e8573b3e02cfd9edb74cb7ab09de1a708b30
erikvanmechelen <erik.vanmechelen@gmail.com>
2019-12-16 19:04:09 -0600

    Adding Skillshare Practice Files, including Calculator Project

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+// calculator
+
+function Calculator() {
+    var answer = document.querySelector('#answer');
+    var num1 = Number(document.querySelector('#num1').value);
+    var num2 = Number(document.querySelector('#num2').value);
+    var select = document.querySelector('select');
+    var operator = select.options[select.selectedIndex].value;
+    var theCalculatedAnswer;
+
+    if(operator == 'add') {
+        theCalculatedAnswer = num1 + num2;
+    }
+    else if(operator == 'minus') {
+        theCalculatedAnswer = num1 - num2;
+    }
+    else if(operator == 'divide') {
+        theCalculatedAnswer = num1 / num2;
+    }
+    else if(operator == 'multiply') {
+        theCalculatedAnswer = num1 * num2;
+    } else {
+        // alert('Please select an operator');
+        theCalculatedAnswer = 'Invalid operator'
+    }
+
+    answer.innerHTML = theCalculatedAnswer
+
+}
+
